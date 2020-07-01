4e58e555873056c7e3647db59e3ef2726c1f2cc7
YYYlh <1184262331@qq.com>
2020-07-01 18:00:15 +0800

    优化代码

--- src/generate_vue/index.js
+++ src/generate_vue/index.js
@@ -1,18 +1,14 @@
 const path = require('path')
+const fs = require('fs')
 const template = require('./template')
 const write = require('../file/write')
-const dir = require('../file/dir')
 const process = require('process')
 const pwdDir = process.cwd()
 const viewsDir = path.join(pwdDir, '/src/views')
 const componentsDir = path.join(pwdDir, '/src/components')
 
-async function isDirExits(path, name) {
-    const dirs = await dir(path)
-    return dirs.includes(name)
-}
-
 module.exports = function(type, name) {
+
     const tempalteStr = template(name)
     const stategry = {
         view: { pathUrl: path.join(viewsDir, name), dir: viewsDir },
@@ -21,12 +17,9 @@ module.exports = function(type, name) {
         c: { pathUrl: path.join(componentsDir, name), dir: componentsDir }
     }
     const check = stategry[type]
-    isDirExits(check.dir, name).then(isExits => {
-        if (isExits) {
-            console.log(`${name}已存在`);
-        } else {
-            write(tempalteStr, check.pathUrl, 'index.vue')
-        }
-    })
-    
+    if (fs.existsSync(path.join(check.dir, name))) {
+        console.log(`${name}已存在`);
+    } else {
+        write(tempalteStr, check.pathUrl, 'index.vue')
+    }
 }
