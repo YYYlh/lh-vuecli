1589136941c9082cf2e72fc49a2682c4cc54bcb6
YYYlh <1184262331@qq.com>
2020-07-01 18:00:28 +0800

    去掉打印语句

--- src/file/read.js
+++ src/file/read.js
@@ -1,10 +1,7 @@
 const fs = require('fs')
 function readFile(path) {
-  console.log(path);
   return new Promise((reslove, reject) => {
     fs.readFile(path, (err, data) => {
-      console.log('err: ', err);
-      
       if (err) {
         reject('fail')
       } else {
