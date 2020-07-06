8a83425cacfb70e7935c8b4631bbfc87624878af
YYYlh <liuhao@MacBook-Pro.local>
2020-07-06 22:13:07 +0800

    提交代码

--- src/test/index.js
+++
@@ -1,5 +0,0 @@
-const astEscodegen= require('../ast/escodegen')
-const { importTemplate, programBodyTemplate } = require('../ast/template')
-let aaa = importTemplate(['a'], './index.js')
-let ast = programBodyTemplate([aaa])
-console.log(astEscodegen(ast));
