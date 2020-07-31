d0f4629bca7ab0b8b66b3e79b8719cceb3496795
YYYlh <1184262331@qq.com>
2020-07-31 22:57:37 +0800

    修改版本号

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const generateVue = require('./src/generate_vue')
 const process = require('process')
 
 program
-  .version('1.0.0')
+  .version('1.0.2')
   
 
 program
--- src/file/write.js
+++ src/file/write.js
@@ -3,8 +3,7 @@ const fs = require('fs')
 function writeFile(data, path, fileName) {
   return new Promise((reslove, reject) => {
     // 首先创建目录
-    fs.mkdir(path, { recursive: true }, err => {
-      if (err) throw err
+    if (fs.existsSync(path)) {
       fs.writeFile(`${path}/${fileName}`, data, err => {
         if (err) {
           reject('fail')
@@ -12,7 +11,19 @@ function writeFile(data, path, fileName) {
         }
         reslove('success')
       })
-    })   
+    } else {
+      fs.mkdir(path, { recursive: true }, err => {
+        if (err) throw err
+        fs.writeFile(`${path}/${fileName}`, data, err => {
+          if (err) {
+            reject('fail')
+            throw err
+          }
+          reslove('success')
+        })
+      })   
+    }
+    
   })
 }
 module.exports = writeFile
